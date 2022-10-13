f320b379e472500f44ccb2c83c003b26019390fd
Vardan Melkumyan <trilodi@gmail.com>
2022-10-13 16:49:27 -0400

    iphone save dialog when clicking download

--- script.js
+++ script.js
@@ -201,18 +201,12 @@ const copyLink = () => {
 }
 
 const download = () => {
-    console.log(imgContainerDiv.querySelectorAll('div')[1].querySelector('img'))
-    imgContainerDiv.querySelectorAll('div')[1].querySelector('img').dispatchEvent(new Event("mousedown"));
-    setTimeout(()=> {
-        imgContainerDiv.querySelectorAll('div')[1].querySelector('img').dispatchEvent(new Event("mouseup"));
-        console.log('done')
-    }, 2000)
-    // const downloadLink = document.createElement('a');
-    // downloadLink.href = slides[currentSlideIndex].largeImageURL;
-    // downloadLink.download = slides[currentSlideIndex].largeImageURL;
-    // document.body.append(downloadLink);
-    // downloadLink.click();
-    // downloadLink.remove()
+    const downloadLink = document.createElement('a');
+    downloadLink.href = slides[currentSlideIndex].largeImageURL;
+    downloadLink.download = slides[currentSlideIndex].largeImageURL;
+    document.body.append(downloadLink);
+    downloadLink.click();
+    downloadLink.remove()
 }
 
 const reload = () => {
